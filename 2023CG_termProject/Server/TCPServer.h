d357f098d75add550eb8c5a82ca0668245f48bbc
joke3829 <kim1234312@gmail.com>
2024-11-10 12:48:44 +0900

    첨삭

--- 2023CG_termProject/Server/TCPServer.h
+++ 2023CG_termProject/Server/TCPServer.h
@@ -1,8 +1,23 @@
+//======================================================
+// 수정사항
+// 11.09 생성자를 이용해 초기화 -> BindAndListen()을 사용
+//======================================================
 #pragma once
 #include "stdafx.h"
 
-class TCPServer{
+class TCPServer {
 public:
-private:
-};
+    TCPServer() {};
+    ~TCPServer();
 
+    void BindAndListen();
+    void Execute();
+
+private:
+    SOCKET listen_sock = NULL;
+    std::vector<SOCKET> client_sockets;
+    std::vector<HANDLE> client_threads;
+
+    void AcceptClients();
+    static DWORD WINAPI ClientThread(LPVOID clientSocket);
+};
--- 2023CG_termProject/Server/stdafx.h
+++ 2023CG_termProject/Server/stdafx.h
@@ -2,6 +2,11 @@
 
 #include <WinSock2.h>
 #include <windows.h>
+#include <WS2tcpip.h>
 #include <iostream>
 #include <vector>
 #include <thread>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define Portnum  9000
